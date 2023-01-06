4f4a46bde3ee35a380b81e25a2f3b49dee7b5c00
Tyler Joly <up21995@bristol.ac.uk>
2023-01-06 22:38:33 +0000

    added another function to data.py that works and importing does work

--- modules/data.py
+++ modules/data.py
@@ -5,6 +5,15 @@ class Data:
         
     def make_df(self):
         import pandas as pd
-        dataframe = pd.read_csv(self.filepath, index_col = "time", parse_dates = True)
+        dataframe = pd.read_csv(self.filepath, parse_dates = True)
         return dataframe
+
+    def plot(self):
+        import matplotlib.pyplot as plt
+        import numpy as np
+        fig, ax = plt.subplots()
+        
+        plotdata = self.make_df()
+        plt.plot(plotdata["time"], plotdata["mf"])  # Plot the chart
+        plt.show()  # display
         
