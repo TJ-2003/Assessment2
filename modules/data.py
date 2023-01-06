9cc5ddeafca2f30c99fafc77af790cc9e6479639
Tyler Joly <up21995@bristol.ac.uk>
2023-01-06 21:19:26 +0000

    added code to data.py that works in a testing notebook but doent work when i try and import it

--- modules/data.py
+++ modules/data.py
@@ -1,5 +1,10 @@
 ## add code for data class here
 class Data:
-    def __init__(self, filename):
-        self.filename = filename
+    def __init__(self, filepath):
+        self.filepath = filepath
+        
+    def make_df(self):
+        import pandas as pd
+        dataframe = pd.read_csv(self.filepath, index_col = "time", parse_dates = True)
+        return dataframe
         
