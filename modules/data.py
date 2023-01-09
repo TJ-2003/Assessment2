40de225544556c0744d49b9bc107beaa4e0caf79
Tyler Joly <up21995@bristol.ac.uk>
2023-01-09 16:54:07 +0000

    added code to data.py that extracts the sitecode from the filepath

--- modules/data.py
+++ modules/data.py
@@ -3,6 +3,21 @@ class Data:
     def __init__(self, filepath):
         self.filepath = filepath  # saves the file path that is used in other funtions
         
+        # code which extracts the site code from the filepath:
+        
+        self.sitecode = ""  # set the sitecode to nothing to be reassigned
+        
+        sitecode_extract = filepath[5]+filepath[6]+filepath[7]  # selects the characters from the filepath string which corresponds to the sitecode
+        
+        # sets self.sitecode to give full description of site
+        if sitecode_extract == "mhd": 
+            self.sitecode = "MHD: Mace Head, W. coast of the Republic of Ireland ect."
+        elif sitecode_extract == "tac":
+            self.sitecode = "TAC: Tacolneston, East Anglia"
+            
+        
+        self.species = filepath[9]+filepath[10]+filepath[11]
+        
         import pandas as pd
         df = pd.read_csv(filepath, index_col = "time", parse_dates = True)  # makes a dataframe with data in it
         self.data = df  # saving the new dataframe
